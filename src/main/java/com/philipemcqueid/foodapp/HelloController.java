1b2772f3e6d896615d74279873613ab4f287cc8e
philmcqueid <philmcqueid@gmail.com>
2024-02-07 08:08:08 -0300

    Primeiro commit (iniciando o projeto)

---
+++ src/main/java/com/philipemcqueid/foodapp/HelloController.java
@@ -0,0 +1,15 @@
+package com.philipemcqueid.foodapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String sayHello() {
+        return "Hello World";
+    }
+}
